fa5a681d9ad83c4bc8971dbe94fcfcb8ee67a049
Rickard Ekman <xm0rfin@gmail.com>
2017-12-03 15:56:37 +0100

    Day 2 Part 2

--- day2/index.js
+++ day2/index.js
@@ -1,17 +1,52 @@
 const fs = require("fs");
 const readline = require("readline");
 
-let checksum = 0;
+// Part 1
 
-const lineReader = readline.createInterface({
-  input: fs.createReadStream("./input.txt")
-});
+const part1 = () => {
+  let checksum = 0;
 
-lineReader.on("line", line => {
-  const row = line.split("\t");
-  const min = Math.min.apply(null, row);
-  const max = Math.max.apply(null, row);
-  checksum += max - min;
-});
+  const lineReader = readline.createInterface({
+    input: fs.createReadStream("./input.txt")
+  });
 
-lineReader.on("close", () => console.log(checksum));
+  lineReader.on("line", line => {
+    const row = line.split("\t");
+    const min = Math.min.apply(null, row);
+    const max = Math.max.apply(null, row);
+    checksum += max - min;
+  });
+
+  lineReader.on("close", () => console.log("Part 1:", checksum));
+};
+
+part1();
+
+// Part 2
+
+const part2 = () => {
+  let checksum = 0;
+
+  const lineReader = readline.createInterface({
+    input: fs.createReadStream("./input.txt")
+  });
+
+  lineReader.on("line", line => {
+    const row = line.split("\t");
+
+    row.forEach(number => {
+      for (i = 0; i < row.length; i++) {
+        const division = number / row[i];
+        if (division !== 1 && division % 1 == 0) {
+          checksum += division;
+        }
+      }
+    });
+  });
+
+  lineReader.on("close", () => {
+    console.log("Part 2:", checksum);
+  });
+};
+
+part2();
